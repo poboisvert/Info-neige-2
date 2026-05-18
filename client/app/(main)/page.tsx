900cf77058ff353b4b6095258982966d3d58e51e
PO Boisvert <pob944@hotmail.com>
2026-05-18 18:35:56 -0400

    add readme

--- client/app/(main)/page.tsx
+++ client/app/(main)/page.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from "next";
+
 import { Hero } from "@/components/hero";
 import { WhyChooseUs } from "@/components/why-choose-us";
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "https://neige.app",
+  },
+};
 
 export default function Home() {
   return (
